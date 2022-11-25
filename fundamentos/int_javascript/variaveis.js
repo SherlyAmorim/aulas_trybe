f1babd7ef2a3e03ca52e79149959762ff6ee7853
SherlyAmorim <sherlyamorim@gmail.com>
2022-11-24 22:48:44 -0300

    Treinando com explicação de variáveis

---
+++ fundamentos/int_javascript/variaveis.js
@@ -0,0 +1,12 @@
+const myName = 'Sherly';
+const birthCity = 'Vitória';
+let birthYear = 1985;
+
+console.log ('Meu nome é ' + myName + ', nasci em ' + birthCity + ' no ano de ' + birthYear +'.');
+
+birthYear = 2030;
+// birthCity = 'Vila Velha';
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
