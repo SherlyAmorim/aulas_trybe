26102d99347a280bb10d659617f3209739dae112
SherlyAmorim <sherlyamorim@gmail.com>
2022-12-07 12:49:02 -0300

    Praticando getElementsByClassName em HTML_JS

---
+++ fundamentos/js_domSeletores/getElemsByClassName.js
@@ -0,0 +1,6 @@
+document.getElementsByClassName('piloto-f1-atual')[0].innerText = 'Stroll';
+
+let pilotoName = document.getElementsByClassName('piloto-f1-atual');
+for (let index = 0; index < pilotoName.length; index += 1){
+    pilotoName[index].innerText = 'Bonitões kkk';
+}
