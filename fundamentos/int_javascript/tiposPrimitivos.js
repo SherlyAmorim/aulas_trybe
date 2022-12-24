3b332e4dc4570d95256e938d2ab1cce30bfcdce2
SherlyAmorim <sherlyamorim@gmail.com>
2022-12-24 15:40:30 -0300

    Treinando Tipos Primitivos

--- fundamentos/int_javascript/tiposPrimitivos.js
+++ fundamentos/int_javascript/tiposPrimitivos.js
@@ -1,3 +1,22 @@
+// TESTE
+
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail);
+console.log(typeof patientAge);
+
+
+// PARA FIXAR
+
 const base = 5;
 const heigth = 8;
 const area = (base * heigth);
