9d472c171b68c185ca68611b032db1ba00403d3c
SherlyAmorim <sherlyamorim@gmail.com>
2023-01-11 12:11:18 -0300

    Criando doc.js e enviado os scripts que estavam no html para o js

---
+++ fundamentos/js_domSeletores/getElemByClassTag_01.js
@@ -0,0 +1,9 @@
+const paragraphs = document.getElementsByClassName('para');
+
+paragraphs[0].style.fontSize = '1.5rem';
+
+paragraphs[0].style.color = 'purple';
+
+const subtitle = document.getElementsByTagName('h4')[0];
+
+subtitle.style.color = 'green';
