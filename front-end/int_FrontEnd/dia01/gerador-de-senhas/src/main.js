6bfc0ad9794f5535bbd4f3ca099eb784fabae6cc
SherlyAmorim <sherlyamorim@gmail.com>
2023-02-08 18:30:02 -0300

    Criando html e Js de Gerador de Senhas

--- front-end/int_FrontEnd/dia01/gerador-de-senhas/src/main.js
+++ front-end/int_FrontEnd/dia01/gerador-de-senhas/src/main.js
@@ -1,1 +1,10 @@
+import { nanoid } from "nanoid";
+
+const passwordBtnEl = document.querySelector('button');
+const displayPasswordEl = document.querySelector('h2');
+
+passwordBtnEl.addEventListener('click', () => {
+  const randomPassword = nanoid();
+  displayPasswordEl.innerHTML = randomPassword;
+});
 console.log('Hello World');
