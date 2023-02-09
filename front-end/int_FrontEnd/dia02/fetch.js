53443008832a767f0ff17390d5c1edca5e852094
SherlyAmorim <sherlyamorim@gmail.com>
2023-02-09 20:58:01 -0300

    Pequena alteração do Fetch.js

--- front-end/int_FrontEnd/dia02/fetch.js
+++ front-end/int_FrontEnd/dia02/fetch.js
@@ -1,6 +1,6 @@
 fetch("https://api.goprogram.ai/inspiration")
-  .then(response => response.json())
-  .then(data => console.log(`"${data.quote}" | ${data.author}`));
+  .then((response) => response.json())
+  .then((data) => console.log(`"${data.quote}" | ${data.author}`));
 
 
 
