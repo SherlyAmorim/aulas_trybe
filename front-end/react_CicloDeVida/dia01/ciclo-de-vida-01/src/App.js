4d50e055ba2935c75b596efbd804dc16fbe75d45
SherlyAmorim <sherlyamorim@gmail.com>
2023-03-09 15:46:18 -0300

    Entendendo quando cada método é chamado

---
+++ front-end/react_CicloDeVida/dia01/ciclo-de-vida-01/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Counter from './Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Sherly</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
